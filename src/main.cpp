87b9448ae762bec198bdd93fcde36ff917191d24
Shawn Silverman <shawn@pobox.com>
2018-11-12 22:47:32 -0800

    Adding DMX transmit to main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,19 @@
 
 namespace teensydmx = ::qindesign::teensydmx;
 
+// Create the DMX transmitter on Serial1.
+teensydmx::Sender dmxTx{Serial1};
+
 void setup() {
   // Initialize the serial port and wait for initialization to complete
   Serial.begin(115200);
   while (!Serial && millis() < 4000) {
   }
   Serial.println("Hello, DMX World!");
+
+  // dmxTx.setPacketSize(25);
+  // dmxTx.setRefreshRate(40);
+  dmxTx.begin();
 }
 
 void loop() {
