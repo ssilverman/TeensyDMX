094444d5cb30f86d3ec089e65724b3ba97e6e412
Shawn Silverman <shawn@pobox.com>
2018-11-13 17:36:29 -0800

    Updating the main.cpp program to be similar to the Chaser example.

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,37 @@ namespace teensydmx = ::qindesign::teensydmx;
 // Create the DMX transmitter on Serial1.
 teensydmx::Sender dmxTx{Serial1};
 
+int channel = 0;
+
 void setup() {
   // Initialize the serial port and wait for initialization to complete
   Serial.begin(115200);
   while (!Serial && millis() < 4000) {
   }
+
+  // Set up any pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWriteFast(LED_BUILTIN, LOW);
+  pinMode(2, OUTPUT);
+  digitalWriteFast(2, HIGH);
+
   Serial.println("Hello, DMX World!");
 
   // dmxTx.setPacketSize(25);
   // dmxTx.setRefreshRate(40);
   dmxTx.begin();
+  channel = 512;
 }
 
 void loop() {
+  digitalWriteFast(LED_BUILTIN, HIGH);
+  dmxTx.set(channel++, 0);
+  if (channel > 512) {
+    channel = 1;
+  }
+  dmxTx.set(channel, 255);
+  delay(20);
+  digitalWriteFast(LED_BUILTIN, LOW);
+
+  delay(980);
 }
