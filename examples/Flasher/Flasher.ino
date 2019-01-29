76042f1d2252fd4e44a35974e89459cd07032e43
Shawn Silverman <shawn@pobox.com>
2019-01-29 12:25:06 -0800

    Updating the Flasher example to have better structure and more constant variables instead of constant numbers.

--- examples/Flasher/Flasher.ino
+++ examples/Flasher/Flasher.ino
@@ -7,28 +7,39 @@
 
 #include <TeensyDMX.h>
 
+namespace teensydmx = ::qindesign::teensydmx;
+
 // DMX channel from which to read the flash speed.
 constexpr int kChannel = 1;
 
 // Timeout after which it's considered that DMX is no longer sending.
-constexpr unsigned long kDMXTimeout = 1000;
+constexpr unsigned long kDMXTimeout = 1000;  // 1s
 
-// The pin for which to flash the LED.
+// The LED pin.
 constexpr uint8_t kLEDPin = LED_BUILTIN;
 
 // Interval between printing received values.
-constexpr unsigned long kPrintInterval = 1000;
+constexpr unsigned long kPrintInterval = 1000;  // 1s
 
-namespace teensydmx = ::qindesign::teensydmx;
+// Constants for flashing rate
+constexpr int32_t kPeriodMax = 1000;  // 1s
+constexpr int32_t kPeriodMin = 30;    // 30ms
 
 // Create the DMX receiver on Serial1.
 teensydmx::Receiver dmxRx{Serial1};
 
+// Buffer used for reading the DMX data.
+uint8_t buf[1];
+
+// The last value received on kChannel.
+uint8_t lastValue;
+
 // Keeps track of when the last frame was received.
 elapsedMillis lastFrameTimer;
 
-// The last value sent to kChannel.
-uint8_t lastValue;
+// Flashing state
+int32_t period = kPeriodMax;
+int64_t phi = 0;
 
 void setup() {
   // Initialize the serial port
@@ -36,19 +47,21 @@ void setup() {
   while (!Serial && millis() < 4000) {
     // Wait for initialization to complete or a time limit
   }
-  Serial.println("Starting.");
+  Serial.println("Starting Flasher.");
 
   // Set up any pins
   pinMode(kLEDPin, OUTPUT);
+  digitalWriteFast(kLEDPin, HIGH);  // Start with the LED on
 
   dmxRx.begin();
   lastFrameTimer = kDMXTimeout;
 }
 
 void loop() {
-  static uint8_t buf[1];
   int read = dmxRx.readPacket(buf, kChannel, 1);
+  int64_t t = millis();
   if (read > 0) {
+    // We've read everything we want to
     lastValue = buf[0];
     lastFrameTimer = 0;
 
@@ -58,21 +71,18 @@ void loop() {
       Serial.printf("%d: %d\n", dmxRx.packetCount(), lastValue);
       p = 0;
     }
-  }
 
-  if (lastFrameTimer < kDMXTimeout) {
-    // Use a wave equation to make the speed-ups and slow-downs smoother
-    // using the offset, phi
-    static int32_t period = 1000;
-    static int64_t phi = 0 * period;
-    int32_t newPeriod = map(lastValue, 0, 255, 1000, 30);  // T range is 1s to 30ms
-    int64_t t = millis();
+    // Use a wave equation to make the speed-ups and slow-downs smoother using
+    // the offset, phi
+    int32_t newPeriod = map(lastValue, 0, 255, kPeriodMax, kPeriodMin);
     if (newPeriod != period) {
       phi = (t*(period - newPeriod) + newPeriod*phi)/period;
       period = newPeriod;
     }
-    int32_t v = -(phi - t)%period;
+  }
 
+  if (lastFrameTimer < kDMXTimeout) {
+    int32_t v = (t - phi)%period;
     if (v < period/2) {
       digitalWriteFast(kLEDPin, HIGH);
     } else {
