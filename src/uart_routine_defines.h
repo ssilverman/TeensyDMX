00aacca5e0f9402c545dfe1eeabc5c472922b00c
Shawn Silverman <shawn@pobox.com>
2019-12-14 19:32:31 -0800

    Splitting the UART_TX_DATA_STATE_NO_FIFO #define into two macros: UART_TX_DATA_STATE_NO_FIFO and LPUART_TX_DATA_STATE_NO_FIFO that take the register number as a parameter.

--- src/uart_routine_defines.h
+++ src/uart_routine_defines.h
@@ -22,14 +22,26 @@
     UART##N##_D = instance->outputBuf_[instance->outputBufIndex_++]; \
   } while (UART##N##_TCFIFO < 8);
 
-#define UART_TX_DATA_STATE_NO_FIFO(CTRL, DATA, CTRL_PREFIX)   \
-  if (instance->outputBufIndex_ >= instance->packetSize_) {   \
-    CTRL = CTRL_PREFIX##_TX_COMPLETING;                       \
-  } else {                                                    \
-    DATA = instance->outputBuf_[instance->outputBufIndex_++]; \
-    if (instance->outputBufIndex_ >= instance->packetSize_) { \
-      CTRL = CTRL_PREFIX##_TX_COMPLETING;                     \
-    }                                                         \
+// N is the register number.
+#define UART_TX_DATA_STATE_NO_FIFO(N)                                \
+  if (instance->outputBufIndex_ >= instance->packetSize_) {          \
+    UART##N##_C2 = UART_C2_TX_COMPLETING;                            \
+  } else {                                                           \
+    UART##N##_D = instance->outputBuf_[instance->outputBufIndex_++]; \
+    if (instance->outputBufIndex_ >= instance->packetSize_) {        \
+      UART##N##_C2 = UART_C2_TX_COMPLETING;                          \
+    }                                                                \
+  }
+
+// N is the register number.
+#define LPUART_TX_DATA_STATE_NO_FIFO(N)                                   \
+  if (instance->outputBufIndex_ >= instance->packetSize_) {               \
+    LPUART##N##_CTRL = LPUART_CTRL_TX_COMPLETING;                         \
+  } else {                                                                \
+    LPUART##N##_DATA = instance->outputBuf_[instance->outputBufIndex_++]; \
+    if (instance->outputBufIndex_ >= instance->packetSize_) {             \
+      LPUART##N##_CTRL = LPUART_CTRL_TX_COMPLETING;                       \
+    }                                                                     \
   }
 
 // N is the register number.
